a165b7ed203724d950d2fc51daff3004e974ca42
navetacandra <dev@navetacandraa.my.id>
2026-02-02 04:23:10 +0700

    feat: create API service with Hono

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import { Hono } from "hono";
 import {
   getAudioFile,
   getAutomixQueue,
@@ -5,16 +6,37 @@ import {
   getSearchResult,
 } from "./music/music.service";
 
-// Only for test purpose
+const app = new Hono();
 
-const featured = getFeatured();
-console.log(
-  JSON.stringify(
-    await Promise.all([
-      await featured,
-      await getSearchResult("Sheila On 7"),
-      await getAutomixQueue((await featured)[0]!.videoId),
-      await getAudioFile((await featured)[0]!.videoId),
-    ]),
-  ),
+app.get("/health", (c) => c.json({ status: "healthy" }));
+app.get("/", (c) =>
+  c.json({
+    author: "navetacandra",
+    profile: "https://github.com/navetacandra",
+  }),
 );
+
+app.get("/featured", async (c) => c.json({ result: await getFeatured() }));
+
+app.get("/search/:keyword", async (c) =>
+  c.json({
+    keyword: c.req.param("keyword"),
+    result: await getSearchResult(c.req.param("keyword")),
+  }),
+);
+
+app.get("/automix/:id", async (c) =>
+  c.json({
+    musicId: c.req.param("id"),
+    result: await getAutomixQueue(c.req.param("id")),
+  }),
+);
+
+app.get("/audio/:id", async (c) =>
+  c.json({
+    musicId: c.req.param("id"),
+    audioURL: await getAudioFile(c.req.param("id")),
+  }),
+);
+
+export default app;
