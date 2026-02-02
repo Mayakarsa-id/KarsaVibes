7641663ac9726d7c7bc2448efb921c63f4ef059f
navetacandra <dev@navetacandraa.my.id>
2026-02-02 08:17:18 +0700

    refac: splitting API service code

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,6 @@
-import { Hono } from "hono";
-import {
+export {
   getAudioFile,
   getAutomixQueue,
   getFeatured,
   getSearchResult,
 } from "./music/music.service";
-
-const app = new Hono();
-
-app.get("/health", (c) => c.json({ status: "healthy" }));
-app.get("/", (c) =>
-  c.json({
-    author: "navetacandra",
-    profile: "https://github.com/navetacandra",
-  }),
-);
-
-app.get("/featured", async (c) => c.json({ result: await getFeatured() }));
-
-app.get("/search/:keyword", async (c) =>
-  c.json({
-    keyword: c.req.param("keyword"),
-    result: await getSearchResult(c.req.param("keyword")),
-  }),
-);
-
-app.get("/automix/:id", async (c) =>
-  c.json({
-    musicId: c.req.param("id"),
-    result: await getAutomixQueue(c.req.param("id")),
-  }),
-);
-
-app.get("/audio/:id", async (c) =>
-  c.json({
-    musicId: c.req.param("id"),
-    audioURL: await getAudioFile(c.req.param("id")),
-  }),
-);
-
-export default app;
